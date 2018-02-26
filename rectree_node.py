81bbb0c45dcbd8ae78acbaf736c36702bdb2b63b
Falko Goettsch <falko@uw.edu>
2018-02-25 22:02:48 -0800

    added Edge class, has pointer to equivalent Edge, lots of refactoring; doesn't run

--- rectree_node.py
+++ rectree_node.py
@@ -3,20 +3,32 @@
 class Node():
     ''' node for a binary tree of Rectangles '''
 
-    def __init__(self, id, parent, rectangle, a=None, b=None):
+    def __init__(self, id, parent, rectangle, child_a=None, child_b=None):
         self.id = id
 
+        #TODO I don't think we ever use the parent pointer but
+        # it doesn't cost much and can be super-useful
         self.parent = parent
-        self.a = a
-        self.b = b
+        self.a = child_a
+        self.b = child_b
 
         self.rect = rectangle
         self.rect.label = id
 
+        #TODO reference the rect via self.rect or rectangle?
+        self.edges = [self.rect.get_edges()]
+
     def centroid(self):
+        ''' Return the centroid of a Node (floor division)
+        Node.centroid -> XY object '''
+
         return self.rect.orig + self.rect.dims // 2
 
     def __repr__(self):
+        ''' DEBUG: Currently the format is: "Node <id>"
+
+        Format: "<id> - p: (<parent>) a: (<child_a>) b: (<child_b>) rect: <rectangle>"'''
+
         if self.parent is None:
             parent = None
         else:
--- rectree_tree.py
+++ rectree_tree.py
@@ -8,13 +8,15 @@ from PIL import Image, ImageDraw
 
 class Edge():
     ''' a Line and up to 2 Nodes'''
-    def __init__(self, line, nodes):
-        assert len(nodes) <= 2, "Too many nodes: {}".format(nodes)
+    def __init__(self, line, node, same_as=None):
         self.line = line
-        self.nodes = nodes
+        self.nodes = node
+        self.same_as = same_as
 
     def __repr__(self):
-        return "{}: {}".format(self.line, self.nodes)
+        # Don't print __repr__ of the linked Edge to avoid
+        # infinite reflection
+        return "{}: {} aka {}: {}".format(self.line, self.node, self.same_as.line, self.same_as.node))
 
 class Tree():
     ''' a binary tree of Rectangles '''
@@ -24,10 +26,13 @@ class Tree():
         # the highest ID currently in the tree
         self.max_id = 0
 
-        self.segments = {}
-        for line in self.root.rect.get_edges():
-            self.segments[line] = [self.root]
+        # A registry of all Edges in the Tree
+        self.edges = {}
+        for line in self.root.edges:
+            self.edges[line] = [self.root]
 
+        # Store the dimensions of the root node since we'll be
+        # deleting its Rectangle
         self.canvas = rectangle.dims
 
     def get(self, id):
@@ -47,8 +52,8 @@ class Tree():
 
         return walk(self.root, id)
 
-    def add_segment(self, new_line, new_node):
-        print("> Adding line {}".format(new_line))
+    def register_edge(self, new_edge):
+        print("> Registering Edge {}".format(new_edge))
 
         def dovetail(line_a, node_a, line_b, node_b):
             #TODO maybe I should make a segment class?
@@ -76,11 +81,9 @@ class Tree():
             #print("Returning new segment(s): {}".format(new_segments))
             return new_segments
 
-        if new_line in self.segments:
+        if new_edge in self.edges:
             #this exact line is already in the registry
             #just add this node to that segment
-            #print(self.segments)
-            #print(self.segments[new_line])
             assert len(self.segments[new_line]) == 1, "Segment doesn't have exactly 1 node: {}".format(self.segments[new_line])
             self.segments[new_line] += [new_node]
             print("Segment {:19} + {} ({}) just append a node".format(str(new_line), new_node, self.segments[new_line]))
@@ -127,14 +130,19 @@ class Tree():
 
         print("=== Splitting {} (probably into Node {} and Node {})".format(cur, self.max_id + 1, self.max_id + 2))
 
+        #TODO may be time to learn to implement exception handling
+        # Check if node exist
         if cur is None:
             print("Could not find Node {}".format(id))
             return
 
+        # Check if node is a leaf node
         if not ( cur.a is None and cur.b is None ):
             print("You can only split leaf nodes. This node has children: {}".format(cur))
             return
 
+        # If no direction is specified, split across the short dimension or
+        # split randomly if it's a square
         if direction is None:
             if cur.rect.dims.x > cur.rect.dims.y:
                 direction = "v"
@@ -143,6 +151,10 @@ class Tree():
             else:
                 direction = choice(("v", "h"))
 
+        # Split the Rectangle in the matching direction
+        #TODO I once thought that I was glad I made separate splits on the
+        # Rectangle object, but I no longer remember why. It doesn't seem to
+        # fit with future diagonal split support
         if direction.startswith("v"):
             rect_a, rect_b = cur.rect.v_split()
         elif direction.startswith("h"):
@@ -151,33 +163,51 @@ class Tree():
             print("Something went wrong. Direction has to start with 'v' or 'h' but is {}".format(direction))
             return
 
-        # remove the split node's segments from the Tree
+        '''
+            Design Considerations
+                Would like to allow for diagonal splits
+        '''
+
+        #                add Edges to tree's Edge registry
+
         print("Cleaning up edges of {}".format(cur))
-        for line in cur.rect.get_edges():
-            self.segments[line].remove(cur)
-            #print("Segment {:19} - {} ({})".format(str(line), cur, self.segments[line]))
-            if self.segments[line] == []:
-                #print("Deleting empty segment {}".format(line))
-                del self.segments[line]
 
-        #TODO should there be an add_node method?
-        self.max_id += 1
-        new_a_id = self.max_id
-        cur.a = Node(new_a_id, cur, rect_a)
-        for line in cur.a.rect.get_edges():
-            self.add_segment(line, cur.a)
-
-        self.max_id += 1
-        new_b_id = self.max_id
-        cur.b = Node(new_b_id, cur, rect_b)
-        for line in cur.b.rect.get_edges():
-            self.add_segment(line, cur.b)
-
-        #delete the rectangle from the parent node
-        #if merging becomes a thing, this might not be smart
+        # Remove Node's Rectangle
+        #TODO what is the good way to delete/remove/blank things in objects?
         cur.rect = None
+        
+        # Remove Node from Edges in the Tree's registry
+        for edge in cur.edges:
+            #TODO I'm making a dict where keys are Lines and values are Edges, which are
+            # a Line and up to two Nodes. This double-use of the same Line must be stupid?
+            self.segments[edge.line].remove_node(cur)
+            #print("Segment {:19} - {} ({})".format(str(line), cur, self.segments[line]))
+            # If this leaves the Edge empty, delete it from the registry
+            if self.segments[edge.line].isempty():
+                #print("Deleting empty segment {}".format(line))
+                #TODO when referencing the same index multiple times, do I just make a "pointer" to it?
+                del self.segments[edge.line]
 
-        return new_a_id, new_b_id
+        # Delete the Node's Edges
+        cur.edges = None
+
+        #TODO Make an add_node() method or use Node.__init__()?
+        # A method would keep Node agnostic of Tree's ID scheme
+        cur.a = self.add_node(cur, rect_a)
+        cur.b = self.add_node(cur, rect_b)
+
+        return cur.a, cur.b
+
+    def add_node(self, parent, rectangle):
+        self.max_id += 1
+
+        new_node = Node(self.max_id, parent, rectangle)
+
+        # Add the edges to the Tree's registry
+        for edge in new_node.edges:
+            self.register_edge(edge)
+
+        return new_node
 
     def leaves(self, start_id=0):
     # This only shows the id, not the rectangles
@@ -202,8 +232,14 @@ class Tree():
         for segment in self.segments:
             assert len(self.segments[segment]) == 1 or len(self.segments[segment]) == 2, "Segment {} has an impossible number of associated nodes: {} ({})".format(segment, len(self.segments[segment]), self.segments[segment])
 
+            # All Edges are pink
+            # Color all Edges tracked in the registry in blue
+            draw.line(segment.astuples(), fill="blue", width=3)
+
             if len(self.segments[segment]) == 2:
+                # Color all shared Edges green
                 draw.line(segment.astuples(), fill="lightgreen", width=3)
+                # Connect the centroids of adjacent Rectangles
                 centroid_a = self.segments[segment][0].centroid()
                 centroid_b = self.segments[segment][1].centroid()
                 draw.line((centroid_a.astuple(), centroid_b.astuple()), fill="black")
